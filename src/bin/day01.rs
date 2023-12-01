d44f4f14ab6d1d77675a9e89636e23dd76c1ff2a
Brian Dawn <brian.dawn@addium.io>
2023-12-01 13:45:18 -0600

    Add day 01.

---
+++ src/bin/day01.rs
@@ -0,0 +1,70 @@
+fn parse_spelled_numbers(input: &str) -> Vec<u32> {
+    let number_words = [
+        ("one", 1),
+        ("1", 1),
+        ("two", 2),
+        ("2", 2),
+        ("three", 3),
+        ("3", 3),
+        ("four", 4),
+        ("4", 4),
+        ("five", 5),
+        ("5", 5),
+        ("six", 6),
+        ("6", 6),
+        ("seven", 7),
+        ("7", 7),
+        ("eight", 8),
+        ("8", 8),
+        ("nine", 9),
+        ("9", 9),
+    ];
+
+    let mut out = vec![];
+
+    for i in 0..input.len() {
+        for (word, number) in number_words {
+            if input[i..].starts_with(word) {
+                out.push(number);
+                break;
+            }
+        }
+    }
+
+    out
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let input = lib::read_data("day01")?;
+
+    let answer_1: u32 = input
+        .iter()
+        .map(|line| {
+            let digits = line.chars().filter(|c| c.is_digit(10)).collect::<Vec<_>>();
+
+            let first_digit = digits.get(0)?;
+            let last_digit = digits.get(digits.len() - 1)?;
+
+            Some(first_digit.to_digit(10)? * 10 + last_digit.to_digit(10)?)
+        })
+        .flatten()
+        .sum();
+
+    println!("Answer 1: {}", answer_1);
+
+    let answer_2: u32 = input
+        .iter()
+        .map(|line| {
+            let digits = parse_spelled_numbers(line);
+
+            let first_digit = digits.get(0)?;
+            let last_digit = digits.get(digits.len() - 1)?;
+
+            Some(first_digit * 10 + last_digit)
+        })
+        .flatten()
+        .sum();
+
+    println!("Answer 2: {}", answer_2);
+    Ok(())
+}
---
+++ src/common.rs
@@ -0,0 +1,8 @@
+use std::error::Error;
+
+pub fn read_data(day: &str) -> Result<Vec<String>, Box<dyn Error>> {
+    let filename = format!("input/{}.txt", day);
+    let contents = std::fs::read_to_string(filename)?;
+    let lines: Vec<String> = contents.split("\n").map(|s| s.to_string()).collect();
+    Ok(lines)
+}
