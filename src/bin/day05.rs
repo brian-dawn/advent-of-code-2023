0cb37de91fa788fd2c73f3e00c233249e1368efe
Brian Dawn <brian.dawn@addium.io>
2023-12-06 08:28:42 -0800

    Add a super naive and slow part 2.

--- src/bin/day05.rs
+++ src/bin/day05.rs
@@ -1,3 +1,4 @@
+use itertools::{self, Itertools};
 use std::error::Error;
 
 fn parse_paragraph(paragraph: &str) -> Vec<(u64, u64, u64)> {
@@ -68,7 +69,7 @@ fn test_mapping() {
     assert_eq!(mapping(99, &to), 51);
 }
 
-fn part1(input: &str) -> Option<()> {
+fn part2(input: &str) -> Option<()> {
     // Split by paragraph.
     let mut paragraphs = input.split("\n\n");
 
@@ -81,8 +82,46 @@ fn part1(input: &str) -> Option<()> {
     let temperature_to_humidity = parse_paragraph(paragraphs.next()?);
     let humidity_to_location = parse_paragraph(paragraphs.next()?);
 
-    /*
-    Rather than list every source number and its corresponding destination number one by one, the maps describe entire ranges of numbers that can be converted. Each line within a map contains three numbers: the destination range start, the source range start, and the range length. */
+    let mut lowest = std::u64::MAX;
+
+    let mut seeds_by_pairs = vec![];
+    for (seed, range) in seeds.into_iter().tuples() {
+        for i in 0..range {
+            seeds_by_pairs.push(seed + i);
+        }
+    }
+
+    for seed in seeds_by_pairs {
+        // Walk everything till we get to location.
+        let mut num = seed;
+        num = mapping(num, &seed_to_soil);
+        num = mapping(num, &soil_to_fertilizer);
+        num = mapping(num, &fertilizer_to_water);
+        num = mapping(num, &water_to_light);
+        num = mapping(num, &light_to_temperature);
+        num = mapping(num, &temperature_to_humidity);
+        num = mapping(num, &humidity_to_location);
+
+        lowest = std::cmp::min(lowest, num);
+    }
+
+    println!("Part 2: {}", lowest);
+
+    Some(())
+}
+
+fn part1(input: &str) -> Option<()> {
+    // Split by paragraph.
+    let mut paragraphs = input.split("\n\n");
+
+    let seeds = parse_seeds(paragraphs.next()?);
+    let seed_to_soil = parse_paragraph(paragraphs.next()?);
+    let soil_to_fertilizer = parse_paragraph(paragraphs.next()?);
+    let fertilizer_to_water = parse_paragraph(paragraphs.next()?);
+    let water_to_light = parse_paragraph(paragraphs.next()?);
+    let light_to_temperature = parse_paragraph(paragraphs.next()?);
+    let temperature_to_humidity = parse_paragraph(paragraphs.next()?);
+    let humidity_to_location = parse_paragraph(paragraphs.next()?);
 
     let mut lowest = std::u64::MAX;
     for seed in seeds {
@@ -99,7 +138,7 @@ fn part1(input: &str) -> Option<()> {
         lowest = std::cmp::min(lowest, num);
     }
 
-    println!("{}", lowest);
+    println!("Part 1: {}", lowest);
 
     Some(())
 }
@@ -108,5 +147,6 @@ fn main() -> Result<(), Box<dyn Error>> {
     let input = std::fs::read_to_string("input/day05.txt")?;
 
     part1(&input);
+    part2(&input);
     Ok(())
 }
