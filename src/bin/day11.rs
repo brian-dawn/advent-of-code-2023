5d5d5490ef056362cfe433c7b02eccb8df54bc03
Brian Dawn <brian.dawn@addium.io>
2023-12-11 09:01:33 -0600

    Retool things to work better, add part2.

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -1,187 +1,110 @@
 use itertools::Itertools;
-use std::error::Error;
+use std::{collections::HashSet, error::Error};
 
-#[derive(Debug, Clone, PartialEq, Eq)]
-enum Space {
-    Empty,
-    Galaxy(usize),
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+struct Galaxy {
+    id: usize,
+    x: usize,
+    y: usize,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
-struct Grid {
-    data: Vec<Space>,
-    width: usize,
-    height: usize,
-}
+fn from_lines(input: Vec<String>) -> Vec<Galaxy> {
+    let mut galaxies = Vec::new();
 
-impl Grid {
-    fn from_lines(lines: Vec<String>) -> Grid {
-        let mut grid = Grid {
-            data: Vec::new(),
-            width: 0,
-            height: 0,
-        };
-
-        for line in lines {
-            grid.width = line.len();
-            grid.height += 1;
-
-            for c in line.chars() {
-                match c {
-                    '.' => grid.data.push(Space::Empty),
-                    '#' => grid.data.push(Space::Galaxy(0)),
-                    _ => panic!("Invalid input"),
-                }
+    let mut id = 0;
+    for (y, line) in input.iter().enumerate() {
+        for (x, c) in line.chars().enumerate() {
+            if c == '#' {
+                id += 1;
+                galaxies.push(Galaxy { x, y, id });
             }
         }
-
-        grid
     }
+    galaxies
+}
 
-    fn get(&self, x: usize, y: usize) -> Option<&Space> {
-        if x >= self.width || y >= self.height {
-            None
+fn manhattan_distance(
+    g1: &Galaxy,
+    g2: &Galaxy,
+    expansion_rows: &HashSet<&usize>,
+    expansion_cols: &HashSet<&usize>,
+    expansion_amount: usize,
+) -> usize {
+
+    let mut distance = 0;
+
+    // If we cross an empty row or column we expand the distance by 2 instead of 1 as we walk.
+
+    // First walk horizontally
+    let mut x = g1.x;
+
+    while x != g2.x {
+        if expansion_cols.contains(&x) {
+            distance += expansion_amount;
         } else {
-            Some(&self.data[y * self.width + x])
+            distance += 1;
+        }
+        if x < g2.x {
+            x += 1;
+        } else {
+            x -= 1;
         }
     }
 
-    fn insert_empty_row(&mut self, row: usize) {
-        self.height += 1;
-
-        for x in 0..self.width {
-            self.data.insert(row * self.width + x, Space::Empty);
+    // Now walk vertically
+    let mut y = g1.y;
+    while y != g2.y {
+        if expansion_rows.contains(&y) {
+            distance += expansion_amount;
+        } else {
+            distance += 1;
+        }
+        if y < g2.y {
+            y += 1;
+        } else {
+            y -= 1;
         }
     }
 
-    fn insert_empty_column(&mut self, column: usize) {
-        self.width += 1;
-
-        for y in 0..self.height {
-            self.data.insert(y * self.width + column, Space::Empty);
-        }
-    }
-
-    fn pretty_print(&self) {
-        for y in 0..self.height {
-            for x in 0..self.width {
-                match self.get(x, y).unwrap() {
-                    Space::Empty => print!("."),
-                    Space::Galaxy(_) => print!("#"),
-                }
-            }
-            println!();
-        }
-    }
-
-    fn expand(&self) -> Grid {
-        // Any rows or columns without galaxies are twice as big.
-
-        let mut new_grid = self.clone();
-
-        // Work backwards to avoid indexing issues.
-        for y in (0..self.height).rev() {
-            let mut has_galaxy = false;
-            for x in 0..self.width {
-                if let Space::Galaxy(_) = self.get(x, y).expect("Invalid index") {
-                    has_galaxy = true;
-                    break;
-                }
-            }
-
-            if !has_galaxy {
-                new_grid.insert_empty_row(y);
-            }
-        }
-
-        // Now expand columns.
-        for x in (0..self.width).rev() {
-            let mut has_galaxy = false;
-            for y in 0..self.height {
-                if let Space::Galaxy(_) = self.get(x, y).expect("Invalid index") {
-                    has_galaxy = true;
-                    break;
-                }
-            }
-
-            if !has_galaxy {
-                new_grid.insert_empty_column(x);
-            }
-        }
-
-        new_grid
-    }
-}
-
-#[test]
-fn test_expand() {
-    let mut grid = Grid::from_lines(vec![
-        "###".to_string(),
-        "###".to_string(),
-        "###".to_string(),
-    ]);
-
-    grid.insert_empty_row(1);
-
-    let expected = Grid::from_lines(vec![
-        "###".to_string(),
-        "...".to_string(),
-        "###".to_string(),
-        "###".to_string(),
-    ]);
-
-    assert_eq!(grid, expected);
-    assert_eq!(grid.width, 3);
-    assert_eq!(grid.height, 4);
-
-    grid.insert_empty_column(1);
-
-    let expected = Grid::from_lines(vec![
-        "#.##".to_string(),
-        "....".to_string(),
-        "#.##".to_string(),
-        "#.##".to_string(),
-    ]);
-
-    assert_eq!(grid, expected);
-    assert_eq!(grid.width, 4);
-    assert_eq!(grid.height, 4);
+    distance
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let input = common::read_data("day11")?;
 
-    let grid = Grid::from_lines(input);
+    let galaxies = from_lines(input);
 
-    let expanded = grid.expand();
-    expanded.pretty_print();
 
-    // For each pair of galaxies find the manhattan distance between them.
-    let galaxy_coordinates = expanded
-        .data
-        .iter()
-        .enumerate()
-        .filter_map(|(i, space)| match space {
-            Space::Galaxy(_) => Some((i % expanded.width, i / expanded.width)),
-            _ => None,
-        })
-        .collect::<Vec<_>>();
+    let max_y = galaxies.iter().map(|g| g.y).max().ok_or("No galaxies")?;
+    let max_x = galaxies.iter().map(|g| g.x).max().ok_or("No galaxies")?;
 
-    let part1 = galaxy_coordinates
+    let y_set: HashSet<usize> = (0..=max_y).collect();
+    let x_set: HashSet<usize> = (0..=max_x).collect();
+
+    let galaxy_ys = galaxies.iter().map(|g| g.y).collect::<HashSet<_>>();
+    let galaxy_xs = galaxies.iter().map(|g| g.x).collect::<HashSet<_>>();
+
+    let empty_rows = y_set.difference(&galaxy_ys).collect::<HashSet<_>>();
+    let empty_cols = x_set.difference(&galaxy_xs).collect::<HashSet<_>>();
+
+
+    // Now for each pair of galaxies find the manhattan distance, if we cross an empty row or column we expand the distance by 1.
+    let part1 = galaxies
         .iter()
         .combinations(2)
-        .map(|pair| {
-            let (x1, y1) = pair[0];
-            let (x2, y2) = pair[1];
-
-            let dx = if x1 > x2 { x1 - x2 } else { x2 - x1 };
-            let dy = if y1 > y2 { y1 - y2 } else { y2 - y1 };
-
-            dx + dy
-        })
+        .map(|g| manhattan_distance(&g[0], &g[1], &empty_rows, &empty_cols, 2))
         .sum::<usize>();
 
     println!("Part 1: {}", part1);
+
+    // Now part 2 we expand by 1 million.
+    let part2 = galaxies
+        .iter()
+        .combinations(2)
+        .map(|g| manhattan_distance(&g[0], &g[1], &empty_rows, &empty_cols, 1_000_000))
+        .sum::<usize>();
+
+    println!("Part 2: {}", part2);
 
     Ok(())
 }
